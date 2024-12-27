29df264130911f650b5d46716a18848091711ff8
Vertires <166299681+Vertires@users.noreply.github.com>
2024-12-27 21:23:05 +0200

    Update fb_config.js

--- src/config/fb_config.js
+++ src/config/fb_config.js
@@ -10,14 +10,14 @@ const firebaseApp = initializeApp({
   // storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   // messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
   // appId: process.env.REACT_APP_APPID,
-  apiKey: "AIzaSyDYF7jJSGkpaOCHT6I1206Ye8kojuN9lTk",
-  authDomain: "pixvertfun.firebaseapp.com",
-  databaseURL: "https://pixvertfun-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "pixvertfun",
-  storageBucket: "pixvertfun.firebasestorage.app",
-  messagingSenderId: "828927840529",
-  appId: "1:828927840529:web:b55459b0e4f225a9043cb0",
-  measurementId: "G-8FB9QJ9STW"
+    apiKey: "AIzaSyDYF7jJSGkpaOCHT6I1206Ye8kojuN9lTk",
+    authDomain: "pixvertfun.firebaseapp.com",
+    databaseURL: "https://pixvertfun-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "pixvertfun",
+    storageBucket: "pixvertfun.firebasestorage.app",
+    messagingSenderId: "828927840529",
+    appId: "1:828927840529:web:b55459b0e4f225a9043cb0",
+    measurementId: "G-8FB9QJ9STW"
 });
 
 const realtime = getDatabase(firebaseApp);
