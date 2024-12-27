b7830122e3d2d3f6affcb829603f787259fc8a3c
Vertires <166299681+Vertires@users.noreply.github.com>
2024-12-27 22:00:34 +0200

    Update fb_config.js

--- src/config/fb_config.js
+++ src/config/fb_config.js
@@ -1,8 +1,8 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 
-const firebaseApp = initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyDYF7jJSGkpaOCHT6I1206Ye8kojuN9lTk",
   authDomain: "pixvertfun.firebaseapp.com",
   databaseURL: "https://pixvertfun-default-rtdb.europe-west1.firebasedatabase.app",
@@ -11,8 +11,16 @@ const firebaseApp = initializeApp({
   messagingSenderId: "828927840529",
   appId: "1:828927840529:web:b55459b0e4f225a9043cb0",
   measurementId: "G-8FB9QJ9STW"
-});
+};
 
-const realtime = getDatabase(firebaseApp);
-export const auth = getAuth(firebaseApp);
-export default realtime;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const auth = getAuth(app);
+
+// Fetching data from the Realtime Database
+const dbRef = ref(db, '6400_board');
+onValue(dbRef, (snapshot) => {
+  const data = snapshot.val();
+  console.log(data); // Check the data returned from Firebase
+});
