ae9f298ce7303f4917476a9b4bb0c0b5bcf4dc16
Vertires <166299681+Vertires@users.noreply.github.com>
2024-12-27 21:14:50 +0200

    Update fb_config.js

--- src/config/fb_config.js
+++ src/config/fb_config.js
@@ -2,22 +2,16 @@ import { initializeApp } from 'firebase/app'
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
-const firebaseApp = initializeApp({
-  // apiKey: process.env.REACT_APP_APIKEY,
-  // authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  // databaseURL: process.env.REACT_APP_DATABASEURL,
-  // projectId: process.env.REACT_APP_PROJECTID,
-  // storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  // appId: process.env.REACT_APP_APPID,
-  apiKey: "AIzaSyD_rqs-5yJBq_o9KaD--JWHwDa11iqdgdQ",
-  authDomain: "place-firebase.firebaseapp.com",
-  databaseURL: "https://place-firebase-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "place-firebase",
-  storageBucket: "place-firebase.appspot.com",
-  messagingSenderId: "1046026137375",
-  appId: "1:1046026137375:web:5080272e083cc04af58a2a"
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyDYF7jJSGkpaOCHT6I1206Ye8kojuN9lTk",
+  authDomain: "pixvertfun.firebaseapp.com",
+  databaseURL: "https://pixvertfun-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pixvertfun",
+  storageBucket: "pixvertfun.firebasestorage.app",
+  messagingSenderId: "828927840529",
+  appId: "1:828927840529:web:b55459b0e4f225a9043cb0",
+  measurementId: "G-8FB9QJ9STW"
+};
 
 const realtime = getDatabase(firebaseApp);
 export const auth = getAuth(firebaseApp);
