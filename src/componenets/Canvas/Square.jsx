109b2c872c4c244cd00084baf3dba6bcde8953f3
Vertires <166299681+Vertires@users.noreply.github.com>
2024-12-28 16:05:56 +0200

    s

--- src/componenets/Canvas/Square.jsx
+++
@@ -1,8 +0,0 @@
-
-
-export default function Square({id,upadateSquare,color}) {
-    return (
-        <div className="Square" onClick={()=>upadateSquare()} id={id} style={{backgroundColor: color}}>
-        </div>
-    );
-}
